6ee38325fd941b34aeb463ce69505f87e9876a5f
young <young@vnoke.com>
2022-01-18 18:28:11 +0800

    初始化版本
    1. 完成sdk项目

---
+++ README.md
@@ -0,0 +1,5 @@
+# 简介
+**plugin**
+用于android gradle项目插件，让其支持smali和java混合开发
+**sdk**
+用于java开发，标记java代码应该使用什么规则和smali合并
---
+++ sdk/src/main/java/com/mosect/smali/annotation/Delete.java
@@ -0,0 +1,15 @@
+package com.mosect.smali.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 删除类、字段、方法
+ * 此注解将会在生成dex时移除
+ */
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Delete {
+}
